dface4a48545df12866f5c17a559f3dd459cda9c
Andrew Snelgrove <45642597+andrewsnelgrove@users.noreply.github.com>
2019-10-27 20:43:41 -0230

    Work on addAnswer method.

--- SudokuGrid.java
+++ SudokuGrid.java
@@ -49,8 +49,8 @@ public class SudokuGrid
 
     public void addAnswer(int squareID, int answer)
     {
-        //TOP ROW
         //BOX 0
+        //Top Row
         if (squareID == 0){
             theSudokuGrid.get(0).getSmallSquare(0).changeAnswer(answer);
         }
@@ -60,7 +60,29 @@ public class SudokuGrid
         else if (squareID == 2){
             theSudokuGrid.get(0).getSmallSquare(2).changeAnswer(answer);
         }
+        //Mid Row
+        else if (squareID == 9){
+            theSudokuGrid.get(0).getSmallSquare(3).changeAnswer(answer);
+        }
+        else if (squareID == 10){
+            theSudokuGrid.get(0).getSmallSquare(4).changeAnswer(answer);
+        }
+        else if (squareID == 11){
+            theSudokuGrid.get(0).getSmallSquare(5).changeAnswer(answer);
+        }
+        //Bottom Row
+        else if (squareID == 18){
+            theSudokuGrid.get(0).getSmallSquare(6).changeAnswer(answer);
+        }
+        else if (squareID == 19){
+            theSudokuGrid.get(0).getSmallSquare(7).changeAnswer(answer);
+        }
+        else if (squareID == 20){
+            theSudokuGrid.get(0).getSmallSquare(8).changeAnswer(answer);
+        }
+        /** *********************/
         //BOX 1
+        //Top Row
         else if (squareID == 3){
             theSudokuGrid.get(1).getSmallSquare(0).changeAnswer(answer);
         }
@@ -70,16 +92,118 @@ public class SudokuGrid
         else if (squareID == 5){
             theSudokuGrid.get(1).getSmallSquare(2).changeAnswer(answer);
         }
+        //Mid Row
+
+        else if (squareID == 12){
+            theSudokuGrid.get(1).getSmallSquare(3).changeAnswer(answer);
+        }
+        else if (squareID == 13){
+            theSudokuGrid.get(1).getSmallSquare(4).changeAnswer(answer);
+        }
+        else if (squareID == 14){
+            theSudokuGrid.get(1).getSmallSquare(5).changeAnswer(answer);
+        }
+        //Bottom Row
+        else if (squareID == 21){
+            theSudokuGrid.get(1).getSmallSquare(6).changeAnswer(answer);
+        }
+        else if (squareID == 22){
+            theSudokuGrid.get(1).getSmallSquare(7).changeAnswer(answer);
+        }
+        else if (squareID == 23){
+            theSudokuGrid.get(1).getSmallSquare(8).changeAnswer(answer);
+        }
         //BOX 2
+        //Top Row
         else if (squareID == 6){
             theSudokuGrid.get(2).getSmallSquare(0).changeAnswer(answer);
         }
         else if (squareID == 7){
             theSudokuGrid.get(2).getSmallSquare(1).changeAnswer(answer);
         }
-        else if (squareID == 6){
-            theSudokuGrid.get(2).getSmallSquare(0).changeAnswer(answer);
+        else if (squareID == 8){
+            theSudokuGrid.get(2).getSmallSquare(2).changeAnswer(answer);
         }
+        //Mid Row
+        else if (squareID == 15){
+            theSudokuGrid.get(2).getSmallSquare(3).changeAnswer(answer);
+        }
+        else if (squareID == 16){
+            theSudokuGrid.get(2).getSmallSquare(4).changeAnswer(answer);
+        }
+        else if (squareID == 17){
+            theSudokuGrid.get(2).getSmallSquare(5).changeAnswer(answer);
+        }
+        //Bottom Row
+        else if (squareID == 24){
+            theSudokuGrid.get(2).getSmallSquare(6).changeAnswer(answer);
+        }
+        else if (squareID == 25){
+            theSudokuGrid.get(2).getSmallSquare(7).changeAnswer(answer);
+        }
+        else if (squareID == 26){
+            theSudokuGrid.get(2).getSmallSquare(8).changeAnswer(answer);
+        }
+        //BOX 3
+        //Top Row
+        else if (squareID == 27){
+            theSudokuGrid.get(3).getSmallSquare(0).changeAnswer(answer);
+        }
+        else if (squareID == 28){
+            theSudokuGrid.get(3).getSmallSquare(1).changeAnswer(answer);
+        }
+        else if (squareID == 29){
+            theSudokuGrid.get(3).getSmallSquare(2).changeAnswer(answer);
+        }
+        //Mid Row
+        else if (squareID == 36){
+            theSudokuGrid.get(3).getSmallSquare(3).changeAnswer(answer);
+        }
+        else if (squareID == 37){
+            theSudokuGrid.get(3).getSmallSquare(4).changeAnswer(answer);
+        }
+        else if (squareID == 38){
+            theSudokuGrid.get(3).getSmallSquare(5).changeAnswer(answer);
+        }
+        //Bottom Row
+        else if (squareID == 45){
+            theSudokuGrid.get(3).getSmallSquare(6).changeAnswer(answer);
+        }
+        else if (squareID == 46){
+            theSudokuGrid.get(3).getSmallSquare(7).changeAnswer(answer);
+        }
+        else if (squareID == 47){
+            theSudokuGrid.get(3).getSmallSquare(8).changeAnswer(answer);
+        }
+        //BOX 4
+        //Top Row
+        else if (squareID == 30){
+            theSudokuGrid.get(4).getSmallSquare(0).changeAnswer(answer);
+        }
+        else if (squareID == 31){
+            theSudokuGrid.get(4).getSmallSquare(1).changeAnswer(answer);
+        }
+        else if (squareID == 32){
+            theSudokuGrid.get(4).getSmallSquare(2).changeAnswer(answer);
+        }
+        //Mid Row
+        else if (squareID == 39){
+            theSudokuGrid.get(4).getSmallSquare(3).changeAnswer(answer);
+        }
+        else if (squareID == 40){
+            theSudokuGrid.get(4).getSmallSquare(4).changeAnswer(answer);
+        }
+        else if (squareID == 41){
+            theSudokuGrid.get(4).getSmallSquare(5).changeAnswer(answer);
+        }
+        //LAST STOPPED HERE
+        else if (squareID == 20){
+            theSudokuGrid.get(0).getSmallSquare(8).changeAnswer(answer);
+        }
+        else if (squareID == 20){
+            theSudokuGrid.get(0).getSmallSquare(8).changeAnswer(answer);
+        }
+
 
     }
 
