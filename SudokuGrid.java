7d02339b3cd912563b9c3363c4e41beecf8a2801
Andrew Snelgrove <45642597+andrewsnelgrove@users.noreply.github.com>
2019-12-12 22:06:07 -0330

    Make mapRowToSquareID function

--- SudokuGrid.java
+++ SudokuGrid.java
@@ -267,6 +267,42 @@ public class SudokuGrid {
         navToSquare(squareID).removePossibleAnswer(possToRemove);
     }
 
+
+    //SOLVING THE PUZZLE METHODS
+
+    public void solveSudokuPuzzle(){
+
+
+    }
+
+    private void mapRowStartToSquareID(int rowNumber){
+
+        if (rowNumber == 0) { return 0 }
+        else if (rowNumber == 1) { return 9 }
+        else if (rowNumber == 2) { return 18 }
+        else if (rowNumber == 3) { return 27 }
+        else if (rowNumber == 4) { return 36}
+        else if (rowNumber == 5) { return 45}
+        else if (rowNumber == 6) { return 54}
+        else if (rowNumber == 7) { return 63}
+        else if (rowNumber == 8) { return 72}_
+
+        }
+    }
+
+    private boolean isNumInRow(int numberToCheck, int rowToCheck):
+        for (int column = 0; column <= 8; column++){
+            if (numberToCheck == )
+
+    }
+
+
+
+
+
+
+
+
 /*
 
     public boolean isNumInRowAns(int row, int number)
